624052aea31bc194f782c5b692d18e2ec79f2374
Khye Chun <khyechun@gmail.com>
2023-12-19 23:12:21 +0800

    Add empty apiSlice to store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import appReducer from './redux/appSlice'
+import { apiSlice } from './services/apiSlice'
 
 export default configureStore({
   reducer: {
     app: appReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
 })
