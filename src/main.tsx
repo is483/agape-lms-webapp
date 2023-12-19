d2fb81b898d4bbd8b72bbe930dde479a8081ffe7
Khye Chun <khyechun@gmail.com>
2023-12-19 22:43:29 +0800

    Remove navbarSlice and move store to app directory

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import App from './app'
 import './index.css'
-import store from './store'
+import store from './app/store'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
