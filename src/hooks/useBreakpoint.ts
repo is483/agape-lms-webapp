447d6294e140b3f1dfd491f4d75268cc853f4cd1
Khye Chun <khyechun@gmail.com>
2024-01-05 00:41:48 +0800

    Create custom hook for checking breakpoints above and below specified value

---
+++ src/hooks/useBreakpoint.ts
@@ -0,0 +1,23 @@
+import { useBreakpoint as useBreakpointChakra } from '@chakra-ui/react'
+import { useMemo } from 'react'
+
+const breakpoints = ['base', 'sm', 'md', 'lg', 'xl', '2xl']
+
+function useBreakpoint(breakpoint: 'base' | 'sm' | 'md' | 'lg' | 'xl' | '2xl', up: boolean = true) {
+  const currentBreakpoint = useBreakpointChakra({ ssr: false })
+
+  const isBreakpoint = useMemo(() => {
+    if (!currentBreakpoint) {
+      return true
+    }
+    const breakpointIndex = breakpoints.indexOf(breakpoint)
+    const startIndex = up ? breakpointIndex : 0
+    const endIndex = up ? breakpoints.length - 1 : breakpointIndex
+    const includesBreakpoints = breakpoints.slice(startIndex, endIndex)
+    return includesBreakpoints.includes(currentBreakpoint)
+  }, [breakpoint, currentBreakpoint, up])
+
+  return isBreakpoint
+}
+
+export default useBreakpoint
