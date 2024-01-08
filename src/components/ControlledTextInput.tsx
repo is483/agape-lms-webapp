fe68e092e48443bcd1fb97cecf2a77708324f469
Khye Chun <khyechun@gmail.com>
2024-01-08 17:16:06 +0800

    Add controlled text input component

---
+++ src/components/ControlledTextInput.tsx
@@ -0,0 +1,66 @@
+import {
+  Box, InputGroup, InputLeftElement, Text,
+  Input as ChakraInput,
+  BoxProps,
+  InputProps,
+} from '@chakra-ui/react'
+import { HTMLInputTypeAttribute, forwardRef } from 'react'
+import Icon from './Icon'
+
+interface Props {
+  error: string
+  iconProps?: { name: string } & BoxProps
+  type: HTMLInputTypeAttribute | 'select'
+  placeholder: string
+  boxProps?: BoxProps
+  inputProps?: InputProps
+}
+
+const ControlledTextInput = forwardRef<HTMLInputElement, Props>(({
+  error, type, placeholder, iconProps, boxProps, inputProps,
+}, ref) => {
+  const errorStyleProps = {
+    ...(error && { borderColor: 'red.600', borderWidth: '2px' }),
+  }
+
+  const InputComponent = (
+    <ChakraInput
+      ref={ref}
+      type={type}
+      placeholder={placeholder}
+      {...errorStyleProps}
+      {...inputProps}
+    />
+  )
+
+  const ErrorComponent = <Text position="absolute" fontSize="xs" color="red.600">{error}</Text>
+
+  if (!iconProps) {
+    return (
+      <Box {...boxProps}>
+        {InputComponent}
+        {!!error && ErrorComponent}
+      </Box>
+    )
+  }
+
+  return (
+    <Box {...boxProps}>
+      <InputGroup>
+        <InputLeftElement pointerEvents="none">
+          <Icon {...iconProps} />
+        </InputLeftElement>
+        {InputComponent}
+      </InputGroup>
+      {!!error && ErrorComponent}
+    </Box>
+  )
+})
+
+ControlledTextInput.defaultProps = {
+  boxProps: {},
+  iconProps: undefined,
+  inputProps: {},
+}
+
+export default ControlledTextInput
