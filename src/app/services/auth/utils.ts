6ef2b18196cc24baf294f68d58b4182f02440ebc
Khye Chun <khyechun@gmail.com>
2024-01-12 10:49:12 +0800

    Remove token from store when requests return 403 status

--- src/app/services/auth/utils.ts
+++ src/app/services/auth/utils.ts
@@ -1,18 +1,25 @@
 import { createStandaloneToast } from '@chakra-ui/react'
 import { FetchBaseQueryError } from '@reduxjs/toolkit/query/react'
+import { ThunkDispatch, UnknownAction } from '@reduxjs/toolkit'
 import { router } from '../../../main'
 import paths from '../../../paths'
+import { setToken } from '../../redux/appSlice'
 
 const { toast } = createStandaloneToast()
 
+interface PartialOnQueryStarted {
+  queryFulfilled: Promise<any>
+  dispatch: ThunkDispatch<any, any, UnknownAction>
+}
+
 export const defaultOnQueryStarted = async (
   _arg: any,
-  { queryFulfilled }: { queryFulfilled: Promise<any> },
+  { queryFulfilled, dispatch }: PartialOnQueryStarted,
 ) => {
   queryFulfilled.catch(({ error }) => {
     console.error(error)
     const { status } = error as FetchBaseQueryError
-    handleFetchError(status)
+    handleFetchError(status, dispatch)
   })
 }
 
@@ -20,8 +27,12 @@ export const defaultOnQueryStarted = async (
   Re-usable handler function if using custom onQueryStarted
   function instead of defaultOnQueryStarted
 */
-export const handleFetchError = (status: number | 'FETCH_ERROR' | 'PARSING_ERROR' | 'TIMEOUT_ERROR' | 'CUSTOM_ERROR') => {
+export const handleFetchError = (
+  status: number | 'FETCH_ERROR' | 'PARSING_ERROR' | 'TIMEOUT_ERROR' | 'CUSTOM_ERROR',
+  dispatch: ThunkDispatch<any, any, UnknownAction>,
+) => {
   if (status === 403) {
+    dispatch(setToken(null))
     router.navigate(paths.Login)
   } else {
     toast({
@@ -30,6 +41,7 @@ export const handleFetchError = (status: number | 'FETCH_ERROR' | 'PARSING_ERROR
       status: 'error',
       duration: 9000,
       isClosable: true,
+      position: 'bottom-right',
     })
   }
 }
