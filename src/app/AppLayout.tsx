2df3657fd683efb6700abd19578f51fc0d388fc8
Khye Chun <khyechun@gmail.com>
2024-01-22 12:42:36 +0800

    Add create mentoring journey to routes

--- src/app/AppLayout.tsx
+++ src/app/AppLayout.tsx
@@ -6,6 +6,7 @@ import { getAuth } from './redux/selectors'
 import Introduction from '../features/Onboarding/component/Introduction/Introduction'
 import paths from '../paths'
 import { MentoringJourneys } from '../features/MentoringJourneys'
+import { CreateMentoringJourney } from '../features/MentoringJourneys/CreateMentoringJourney'
 
 function AppLayout() {
   const { role } = useAppSelector(getAuth)
@@ -25,6 +26,7 @@ function AppLayout() {
             <Routes>
               <Route path={paths.Introduction} element={<Introduction />} />
               <Route path={paths.MentoringJourneys.ViewAll} element={<MentoringJourneys />} />
+              <Route path={paths.MentoringJourneys.Create} element={<CreateMentoringJourney />} />
             </Routes>
           )}
           {role === 'Mentee' && (
--- src/features/MentoringJourneys/CreateMentoringJourney/CreateMentoringJourney.tsx
+++ src/features/MentoringJourneys/CreateMentoringJourney/CreateMentoringJourney.tsx
@@ -1,5 +1,15 @@
+import { Text } from '@chakra-ui/react'
+import { Container } from '../../../components'
+
 function CreateMentoringJourney() {
-  return null
+  return (
+    <Container>
+      <Text fontWeight="600">
+        Create Mentoring Journey
+      </Text>
+
+    </Container>
+  )
 }
 
 export default CreateMentoringJourney
