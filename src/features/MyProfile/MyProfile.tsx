46d3030e54163b5e4e6957cac58c098f67985a57
lanceljr <lanceljr98@gmail.com>
2024-01-22 20:06:52 +0800

    Made My Profile page mobile responsive

--- src/features/MyProfile/MyProfile.tsx
+++ src/features/MyProfile/MyProfile.tsx
@@ -1,7 +1,8 @@
 import {
-  Box, useSteps, Hide, Flex, Text,
+  Box, useSteps, Hide, Flex, Text, Accordion, AccordionButton, AccordionItem, AccordionPanel,
 } from '@chakra-ui/react'
 import Container from '../../components/Container'
+import { Icon } from '../../components'
 import { useGetUserInfoQuery } from '../../app/services/user/apiUserSlice'
 import { getAuth } from '../../app/redux/selectors'
 import { useAppSelector } from '../../hooks'
@@ -13,6 +14,9 @@ import PersonalValues from '../Onboarding/component/PersonalValues/PersonalValue
 import MentoringStyle from '../Onboarding/component/MentoringStyle/MentoringStyle'
 import Challenges from '../Onboarding/component/Challenges/Challenges'
 import Interests from '../Onboarding/component/Interests/Interests'
+import useBreakpoint from '../../hooks/useBreakpoint'
+import { Role } from '../../app/types'
+import { TransformedUserResponse } from '../../app/services/user/types'
 
 const steps = [
   { title: 'Personal Information' },
@@ -24,9 +28,220 @@ const steps = [
   { title: 'Interest' },
 ]
 
+interface Props {
+  data: TransformedUserResponse | undefined
+  role: Role | null
+}
 function MyProfile() {
+  const isMdUp = useBreakpoint('md')
   const { role } = useAppSelector(getAuth)
   const { data } = useGetUserInfoQuery({ role: role?.toLowerCase() ?? '' })
+  const MyProfileComponent = isMdUp ? MyProfileDesktop : MyProfileMobile
+  return (
+    <Container>
+      <Text fontSize="2xl" fontWeight="600"> Manage Profile </Text>
+      <Text color="secondary.500" marginTop="1" marginBottom="8">
+        {role === 'Mentor' ? 'Share more about yourself to connect better with your mentees!' : 'Tell us more about yourself so that your mentor can get to know you better!'}
+      </Text>
+      <MyProfileComponent data={data} role={role} />
+    </Container>
+  )
+}
+
+function MyProfileMobile(props: Props) {
+  const { data, role } = props
+  return (
+    <Box>
+      <Accordion allowMultiple>
+        <AccordionItem>
+          {({ isExpanded }) => (
+            <>
+              <h2>
+                <AccordionButton>
+                  <Box as="span" flex="1" textAlign="left">
+                    1. Personal Information
+                  </Box>
+                  {isExpanded ? (
+                    <Icon name="remove" fontWeight="200" fontSize="28px" color={isExpanded ? 'white' : 'secondary.500'}> </Icon>
+                  ) : (
+                    <Icon name="add" fontWeight="200" fontSize="28px"> </Icon>
+                  )}
+                </AccordionButton>
+              </h2>
+              <AccordionPanel pb={4}>
+                <PersonalInformation
+                  minHeight="calc(100vh - 8rem)"
+                  flexDir="column"
+                  justify="space-between"
+                  data={data}
+                />
+              </AccordionPanel>
+            </>
+          )}
+        </AccordionItem>
+        <AccordionItem>
+          {({ isExpanded }) => (
+            <>
+              <h2>
+                <AccordionButton>
+                  <Box as="span" flex="1" textAlign="left">
+                    2. Professional Experience
+                  </Box>
+                  {isExpanded ? (
+                    <Icon name="remove" fontWeight="200" fontSize="28px"> </Icon>
+                  ) : (
+                    <Icon name="add" fontWeight="200" fontSize="28px"> </Icon>
+                  )}
+                </AccordionButton>
+              </h2>
+              <AccordionPanel pb={4}>
+                <ProfessionalExperience
+                  minHeight="calc(100vh - 8rem)"
+                  flexDir="column"
+                  justify="space-between"
+                  data={data}
+                />
+              </AccordionPanel>
+            </>
+          )}
+        </AccordionItem>
+        <AccordionItem>
+          {({ isExpanded }) => (
+            <>
+              <h2>
+                <AccordionButton>
+                  <Box as="span" flex="1" textAlign="left">
+                    3. Skills
+                  </Box>
+                  {isExpanded ? (
+                    <Icon name="remove" fontWeight="200" fontSize="28px"> </Icon>
+                  ) : (
+                    <Icon name="add" fontWeight="200" fontSize="28px"> </Icon>
+                  )}
+                </AccordionButton>
+              </h2>
+              <AccordionPanel pb={4}>
+                <Skills
+                  minHeight="calc(100vh - 8rem)"
+                  flexDir="column"
+                  justify="space-between"
+                  data={data}
+                />
+              </AccordionPanel>
+            </>
+          )}
+        </AccordionItem>
+        <AccordionItem>
+          {({ isExpanded }) => (
+            <>
+              <h2>
+                <AccordionButton>
+                  <Box as="span" flex="1" textAlign="left">
+                    4. Personal Values
+                  </Box>
+                  {isExpanded ? (
+                    <Icon name="remove" fontWeight="200" fontSize="28px"> </Icon>
+                  ) : (
+                    <Icon name="add" fontWeight="200" fontSize="28px"> </Icon>
+                  )}
+                </AccordionButton>
+              </h2>
+              <AccordionPanel pb={4}>
+                <PersonalValues
+                  minHeight="calc(100vh - 8rem)"
+                  flexDir="column"
+                  justify="space-between"
+                  data={data}
+                />
+              </AccordionPanel>
+            </>
+          )}
+        </AccordionItem>
+        <AccordionItem>
+          {({ isExpanded }) => (
+            <>
+              <h2>
+                <AccordionButton>
+                  <Box as="span" flex="1" textAlign="left">
+                    5. Mentoring Style
+                  </Box>
+                  {isExpanded ? (
+                    <Icon name="remove" fontWeight="200" fontSize="28px"> </Icon>
+                  ) : (
+                    <Icon name="add" fontWeight="200" fontSize="28px"> </Icon>
+                  )}
+                </AccordionButton>
+              </h2>
+              <AccordionPanel pb={4}>
+                <MentoringStyle
+                  minHeight="calc(100vh - 8rem)"
+                  flexDir="column"
+                  justify="space-between"
+                  data={data}
+                />
+              </AccordionPanel>
+            </>
+          )}
+        </AccordionItem>
+        <AccordionItem>
+          {({ isExpanded }) => (
+            <>
+              <h2>
+                <AccordionButton>
+                  <Box as="span" flex="1" textAlign="left">
+                    6. Challenges
+                  </Box>
+                  {isExpanded ? (
+                    <Icon name="remove" fontWeight="200" fontSize="28px"> </Icon>
+                  ) : (
+                    <Icon name="add" fontWeight="200" fontSize="28px"> </Icon>
+                  )}
+                </AccordionButton>
+              </h2>
+              <AccordionPanel pb={4}>
+                <Challenges
+                  minHeight="calc(100vh - 8rem)"
+                  flexDir="column"
+                  justify="space-between"
+                  data={data}
+                />
+              </AccordionPanel>
+            </>
+          )}
+        </AccordionItem>
+        <AccordionItem>
+          {({ isExpanded }) => (
+            <>
+              <h2>
+                <AccordionButton>
+                  <Box as="span" flex="1" textAlign="left">
+                    7. Interests
+                  </Box>
+                  {isExpanded ? (
+                    <Icon name="remove" fontWeight="200" fontSize="28px"> </Icon>
+                  ) : (
+                    <Icon name="add" fontWeight="200" fontSize="28px"> </Icon>
+                  )}
+                </AccordionButton>
+              </h2>
+              <AccordionPanel pb={4}>
+                <Interests
+                  minHeight="calc(100vh - 8rem)"
+                  flexDir="column"
+                  justify="space-between"
+                  data={data}
+                />
+              </AccordionPanel>
+            </>
+          )}
+        </AccordionItem>
+      </Accordion>
+    </Box>
+  )
+}
+
+function MyProfileDesktop(props: Props) {
+  const { data, role } = props
   const { activeStep, setActiveStep } = useSteps({
     index: 0,
     count: steps.length,
@@ -37,7 +252,7 @@ function MyProfile() {
   }
 
   return (
-    <Container>
+    <Box>
       <Text fontSize="2xl" fontWeight="600"> Manage Profile </Text>
       <Text color="secondary.500" marginTop="1" marginBottom="8">
         {role === 'Mentor' ? 'Share more about yourself to connect better with your mentees!' : 'Tell us more about yourself so that your mentor can get to know you better!'}
@@ -125,7 +340,7 @@ function MyProfile() {
           </Box>
         </Box>
       </Flex>
-    </Container>
+    </Box>
   )
 }
 
