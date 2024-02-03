13b462535f789f7e64296d4b73052c65a8c0ce66
Khye Chun <khyechun@gmail.com>
2024-02-03 17:37:36 +0800

    Add tabs to mentoring journey details

--- src/features/MentoringJourneys/MentoringJourneyDetails/MentoringJourneyDetails.tsx
+++ src/features/MentoringJourneys/MentoringJourneyDetails/MentoringJourneyDetails.tsx
@@ -1,10 +1,33 @@
+import {
+  Divider, Tab, TabList, TabPanel,
+  TabPanels, Tabs,
+} from '@chakra-ui/react'
 import { BackButton, Container } from '../../../components'
 import paths from '../../../paths'
 
 function MentoringJourneyDetails() {
   return (
-    <Container>
+    <Container position="relative">
       <BackButton path={paths.MentoringJourneys.ViewAll} />
+      <Divider position="absolute" left="0" mt="6" />
+      <Tabs mt="12" colorScheme="red">
+        <TabList gap="6" maxWidth="min-content">
+          <Tab py="1">Overview</Tab>
+          <Tab py="1">Milestones</Tab>
+          <Tab py="1">Feedback</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel px="0">
+            Overview
+          </TabPanel>
+          <TabPanel px="0">
+            Milestones
+          </TabPanel>
+          <TabPanel px="0">
+            Feedback
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
     </Container>
   )
 }
