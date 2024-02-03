a6b7e0575a7b49d428601aadc0b720843ea0f5e0
Khye Chun <khyechun@gmail.com>
2024-02-03 15:02:20 +0800

    Add mentoring journeys table

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -10,8 +10,8 @@ interface ContainerProps extends BoxProps {
 
 function Container({ children, containerProps, ...rest }: ContainerProps) {
   return (
-    <Box padding={['4', null, '8']} background="white" rounded={[0, null, '8']} margin={['0', null, '4']} marginY="4" {...rest}>
-      <ChakraContainer maxW="container.2xl" {...containerProps}>
+    <Box maxW="100%" padding={['4', null, '8']} background="white" rounded={[0, null, '8']} margin={['0', null, '4']} marginY="4" {...rest}>
+      <ChakraContainer maxW="100%" {...containerProps}>
         {children}
       </ChakraContainer>
     </Box>
--- src/features/MentoringJourneys/MentoringJourneys.tsx
+++ src/features/MentoringJourneys/MentoringJourneys.tsx
@@ -1,10 +1,25 @@
 import {
-  Box, Button, Circle, Flex, FlexProps, Tab, TabList, TabPanel, TabPanels, Tabs, Text,
+  Box, Button, Circle, Flex, FlexProps, Tab, TabList, TabPanel, TabPanels, TableContainer, Tabs, Text,
+  Table, Thead, Tbody, Tr, Th, Td, Image, Badge, Spinner,
 } from '@chakra-ui/react'
 import { Container, Icon, Link } from '../../components'
 import paths from '../../paths'
+import { useGetAllMentoringJourneyQuery } from '../../app/services/mentoringJourney/apiMentoringJourneySlice'
+import { MentoringJourney } from '../../app/services/mentoringJourney/types'
+import { MILESTONES } from './CreateMentoringJourney/redux/constants'
+
+enum MentoringJourneyStatus {
+  ONGOING, PAST
+}
 
 function MentoringJourneys() {
+  const { data, isLoading } = useGetAllMentoringJourneyQuery(null)
+
+  const mentoringJourneys = data?.mentoringJourneysAssigned ?? []
+
+  const ongoingMentoringJourneys = mentoringJourneys.filter(({ status }) => status === 'ongoing')
+  const pastMentoringJourneys = mentoringJourneys.filter(({ status }) => status !== 'ongoing')
+
   return (
     <>
       <Container>
@@ -32,11 +47,25 @@ function MentoringJourneys() {
             <Tab py="1">Completed</Tab>
           </TabList>
           <TabPanels>
-            <TabPanel>
-              Ongoing
+            <TabPanel px="0">
+              {
+                isLoading ? (
+                  <Flex justify="center" align="center" height="100px">
+                    <Spinner size="sm" />
+                  </Flex>
+                )
+                  : <MentoringJourneyTable type={MentoringJourneyStatus.ONGOING} data={ongoingMentoringJourneys} />
+              }
             </TabPanel>
-            <TabPanel>
-              Completed
+            <TabPanel px="0">
+              {
+                isLoading ? (
+                  <Flex justify="center" align="center" height="100px">
+                    <Spinner size="sm" />
+                  </Flex>
+                )
+                  : <MentoringJourneyTable type={MentoringJourneyStatus.PAST} data={pastMentoringJourneys} />
+              }
             </TabPanel>
           </TabPanels>
         </Tabs>
@@ -67,6 +96,77 @@ function InfographicItem(props: InfographicItemProps) {
         <Text fontSize={['xl', null, null, '2xl']} color="red.500" lineHeight="1.25" fontWeight="600">{amount}</Text>
       </Box>
     </Flex>
+  )
+}
+
+interface MentoringJourneyTableProps {
+  type: MentoringJourneyStatus
+  data: MentoringJourney[]
+}
+
+function MentoringJourneyTable(props: MentoringJourneyTableProps) {
+  const { type, data } = props
+  return (
+    <TableContainer whiteSpace="unset">
+      <Table variant="simple">
+        <Thead backgroundColor="gray.100">
+          <Tr>
+            <Th>Mentee</Th>
+            <Th>Title</Th>
+            <Th>{type === MentoringJourneyStatus.ONGOING ? 'Milestone' : 'Status'}</Th>
+            <Th />
+          </Tr>
+        </Thead>
+        <Tbody>
+          {data.length === 0 && (
+            <Tr>
+              <Td colSpan={4}>
+                <Flex height="40px" justify="center" align="center">
+                  No mentoring journeys
+                </Flex>
+              </Td>
+            </Tr>
+          )}
+          {data.map((mentoringJourney) => {
+            const {
+              mentee, title, status, milestoneStep, mentoringJourneyId,
+            } = mentoringJourney
+            const { firstName, lastName, profileImgURL } = mentee
+            const fullName = `${firstName} ${lastName}`
+            return (
+              <Tr>
+                <Td>
+                  <Flex gap="2" align="center">
+                    {
+                      profileImgURL
+                        ? <Image src={profileImgURL} borderRadius="100%" maxWidth="100%" width="24px" height="24px" />
+                        : (
+                          <Circle size="24px" bg="secondary.100">
+                            <Icon name="person" color="secondary.300" fontSize="20px" />
+                          </Circle>
+                        )
+                    }
+                    {fullName}
+                  </Flex>
+                </Td>
+                <Td>{title}</Td>
+                <Td>
+                  {
+                    type === MentoringJourneyStatus.ONGOING
+                      ? (
+                        <>{milestoneStep} - {MILESTONES[milestoneStep - 1].title}</>
+                      ) : (
+                        <Badge colorScheme={status === 'complete' ? 'green' : 'red'} textTransform="capitalize">{status}</Badge>
+                      )
+                  }
+                </Td>
+                <Td><Button>View Details</Button></Td>
+              </Tr>
+            )
+          })}
+        </Tbody>
+      </Table>
+    </TableContainer>
   )
 }
 
