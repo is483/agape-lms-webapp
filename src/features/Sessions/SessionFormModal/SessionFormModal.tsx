47e4657e0da7a880ea49460ab34b492c766edb02
lanceljr <lanceljr98@gmail.com>
2024-02-08 21:11:59 +0800

    TH-69 fixed spacing and other UI issues

--- src/features/Sessions/SessionFormModal/SessionFormModal.tsx
+++ src/features/Sessions/SessionFormModal/SessionFormModal.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-param-reassign */
 import {
   Box,
-  Button, Divider, Flex, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay, Radio, RadioGroup, Stack, Text, Textarea,
+  Button, Divider, Flex, HStack, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay, Radio, RadioGroup, SimpleGrid, Stack, Text, Textarea,
 } from '@chakra-ui/react'
 import { ChangeEvent } from 'react'
 import { useImmer } from 'use-immer'
@@ -51,6 +51,7 @@ function CreateSessionModal(props: CreateSessionModalProps) {
   }
 
   const handleModalCancel = () => {
+    updateSession((draft) => clearErrors(draft))
     onModalClose()
   }
 
@@ -98,7 +99,6 @@ function CreateSessionModal(props: CreateSessionModalProps) {
     }
 
     if (hasErrors) {
-
     }
   }
 
@@ -107,10 +107,11 @@ function CreateSessionModal(props: CreateSessionModalProps) {
     <Modal isOpen={isModalOpen} onClose={onModalClose} size="3xl">
       <ModalOverlay />
       <ModalContent p="4" m="4" maxHeight="90vh" overflowY="auto">
-        <ModalHeader>Create Mentoring Session</ModalHeader>
+        <ModalHeader>Create Mentoring Session
+        </ModalHeader>
         <ModalCloseButton />
         <ModalBody>
-          <Flex flexDir="column" mt="6" gap="8">
+          <Flex flexDir="column" mt="4" gap="4">
             <ControlledTextInput
               label="Title"
               type="text"
@@ -124,31 +125,30 @@ function CreateSessionModal(props: CreateSessionModalProps) {
             </Box>
             <Divider orientation="horizontal" />
             <Text fontWeight="600" fontSize="md"> Meeting Details</Text>
-            <Box marginBottom="3">
-              <Stack direction={['column', 'column', 'row']}>
-                <Box>
-                  <Text marginBottom="1">Start Date & Time</Text>
-                  <Input
-                    size="md"
-                    type="datetime-local"
-                    onChange={handleStartDateChange}
-                    value={session.startDate.value}
-                  />
-                  {!!session.startDate.error && <Text fontSize="xs" color="red.600">{session.startDate.error}</Text>}
-                </Box>
-                <Box>
-                  <Text marginBottom="1">End Date & Time</Text>
-                  <Input
-                    size="md"
-                    type="datetime-local"
-                    onChange={handleEndDateChange}
-                    value={session.endDate.value}
-                  />
-                  {!!session.endDate.error && <Text fontSize="xs" color="red.600">{session.endDate.error}</Text>}
-                </Box>
-              </Stack>
-            </Box>
-            <Flex dir="row" alignContent="center" gap="5" marginBottom={session.location.error ? '0' : '8'}>
+            <SimpleGrid columns={[1, 1, 2]} spacing={10} marginBottom="3">
+              <Box>
+                <Text marginBottom="1">Start Date & Time</Text>
+                <Input
+                  size="md"
+                  type="datetime-local"
+                  onChange={handleStartDateChange}
+                  value={session.startDate.value}
+                />
+                {!!session.startDate.error && <Text fontSize="xs" color="red.600">{session.startDate.error}</Text>}
+              </Box>
+
+              <Box>
+                <Text marginBottom="1">End Date & Time</Text>
+                <Input
+                  size="md"
+                  type="datetime-local"
+                  onChange={handleEndDateChange}
+                  value={session.endDate.value}
+                />
+                {!!session.endDate.error && <Text fontSize="xs" color="red.600">{session.endDate.error}</Text>}
+              </Box>
+            </SimpleGrid>
+            <Flex dir="row" alignContent="center" gap="5" marginBottom={session.location.error ? '0' : '3'}>
               <Text fontSize="md">Location: </Text>
               <RadioGroup onChange={handleLocationChange} value={session.location.value}>
                 <Stack direction="row">
@@ -158,12 +158,16 @@ function CreateSessionModal(props: CreateSessionModalProps) {
               </RadioGroup>
             </Flex>
             {!!session.location.error && <Text fontSize="xs" color="red.600" marginBottom="8">{session.description.error}</Text>}
-            <ControlledTextInput
-              label={session.location.value === 'online' ? 'Meeting Link' : 'Meeting Venue'}
-              type="text"
-              error={session.meetingArrangement.error}
-              inputProps={{ onChange: handleMeetingArrangementChange, value: session.meetingArrangement.value }}
-            />
+            <Flex alignItems={['start', 'start', 'center']} gap="3" flexDir={['column', 'column', 'row']} width="100%">
+              <Text> {session.location.value === 'online' ? 'Meeting Link:' : 'Address:'}  </Text>
+              <Box flex="1" w="100%">
+                <ControlledTextInput
+                  type="text"
+                  error={session.meetingArrangement.error}
+                  inputProps={{ onChange: handleMeetingArrangementChange, value: session.meetingArrangement.value }}
+                />
+              </Box>
+            </Flex>
           </Flex>
           <Flex gap="4" justify="flex-end" mt="8">
             <Button colorScheme="red" size="sm" variant="outline" onClick={handleModalCancel}>Cancel</Button>
