8aefbfc5b3b7a47795a71af953cc4d4429ea8ed6
lanceljr <lanceljr98@gmail.com>
2024-02-09 10:59:39 +0800

    TH-64/18 added type for Session response details

--- src/app/services/session/types.ts
+++ src/app/services/session/types.ts
@@ -1,4 +1,18 @@
+export interface Session {
+  sessionId: number
+  status: string
+  fromDateTime: string
+  toDateTime: string
+  sessionType: string
+  location: string
+  notes: string
+  mentoringJourneyId: number
+  title: string
+  description: string
+  reason: string
+}
+
 export interface SessionResponse {
   // TODO: HELP DO THIS LANCE
-
+  sessions: Session[]
 }
