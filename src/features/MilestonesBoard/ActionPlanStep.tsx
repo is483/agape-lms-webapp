21fcc3cbc67acb946800073978c3be6b940dd86c
Khye Chun <khyechun@gmail.com>
2024-02-16 16:37:21 +0800

    Add by who indicator to action plan step UI

--- src/features/MilestonesBoard/ActionPlanStep.tsx
+++ src/features/MilestonesBoard/ActionPlanStep.tsx
@@ -1,5 +1,6 @@
 import {
-  Box, Checkbox, Flex, Text,
+  Badge,
+  Box, Checkbox, Divider, Flex, Text,
 } from '@chakra-ui/react'
 import { useParams } from 'react-router-dom'
 import { ActionPlan } from '../MentoringJourneys/CreateMentoringJourney/redux/types'
@@ -36,23 +37,27 @@ function ActionPlanStep(props: ActionPlanStepProps) {
   }
 
   return (
-    <Flex align="start" mb="2">
-      <Checkbox isChecked={isDone} m="2" mt="1" ml="0" disabled={role === 'Mentee'} onChange={handleIsDoneChange} />
-      <Box flexGrow="1" px="1">
-        <Flex justify="space-between" mb="1">
-          <Text fontSize="sm" fontWeight="bold" textTransform="uppercase">Step {stepNumber}</Text>
-          <Text fontSize="sm" color="gray.500">{deadline}</Text>
-        </Flex>
-        <Text fontSize="xs" fontWeight="500" textTransform="uppercase">Resources Required</Text>
-        <Text fontSize="sm" mb="2" color="gray.500">
-          {resourcesRequired}
-        </Text>
-        <Text fontSize="xs" fontWeight="500" textTransform="uppercase">Progress Indicator Required</Text>
-        <Text fontSize="sm" mb="2" color="gray.500">
-          {progressIndicator}
-        </Text>
-      </Box>
-    </Flex>
+    <Box>
+      <Divider />
+      <Badge my="2">{byWho}</Badge>
+      <Flex align="start" mb="2">
+        <Checkbox isChecked={isDone} m="2" mt="1" ml="0" disabled={role === 'Mentee'} onChange={handleIsDoneChange} />
+        <Box flexGrow="1" px="1">
+          <Flex justify="space-between" mb="1">
+            <Text fontSize="sm" fontWeight="bold" textTransform="uppercase">Step {stepNumber}</Text>
+            <Text fontSize="sm" color="gray.500">{deadline}</Text>
+          </Flex>
+          <Text fontSize="xs" fontWeight="500" textTransform="uppercase">Resources Required</Text>
+          <Text fontSize="sm" mb="2" color="gray.500">
+            {resourcesRequired}
+          </Text>
+          <Text fontSize="xs" fontWeight="500" textTransform="uppercase">Progress Indicator Required</Text>
+          <Text fontSize="sm" mb="2" color="gray.500">
+            {progressIndicator}
+          </Text>
+        </Box>
+      </Flex>
+    </Box>
   )
 }
 
