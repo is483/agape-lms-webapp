d087c573676da817b4733947f74d35b7c7de7e76
khyechun <khyechun@botmd.io>
2024-03-12 14:05:03 +0800

    Add mentor and mentee data to admin feedback view

--- src/features/Feedback/questionnaires/AdminView/MenteeSessionFeedbackAnswers.tsx
+++ src/features/Feedback/questionnaires/AdminView/MenteeSessionFeedbackAnswers.tsx
@@ -1,7 +1,9 @@
 import { useParams } from 'react-router-dom'
-import { Flex, HStack, Text } from '@chakra-ui/react'
+import {
+  Box, Flex, HStack, Text,
+} from '@chakra-ui/react'
 import { useGetMenteeSessionFeedbackQuery } from '../../../../app/services/feedback/apiFeedbackSlice'
-import { Container, Icon } from '../../../../components'
+import { Container, Icon, ProfileIcon } from '../../../../components'
 import QuestionList from '../../components/QuestionList'
 import { MENTOR_SESSION_QUESTIONS } from '../../constants'
 import { SectionWithAnswers } from '../../components/types'
@@ -25,9 +27,8 @@ function MenteeSessionFeedbackAnswers() {
 
   return (
     <>
-      {/* TODO: Add session details */}
       <Container mb="4">
-        <Text fontSize="lg" fontWeight="bold">{sessionData?.sessionDetails.title}</Text>
+        <Text fontSize="lg" fontWeight="600">Mentee Feedback - {sessionData?.sessionDetails.title}</Text>
         <Flex gap={['3', '3', '10']} flexDir={['column', 'column', 'row']} mt={['5', '5', '2']}>
           <HStack>
             <Icon name="calendar_today" fontSize="25px" />
@@ -43,6 +44,30 @@ function MenteeSessionFeedbackAnswers() {
             <Icon name="hourglass_top" fontSize="25px" />
             <Text color="secondary.500"> {getSessionDuration(startDateObject, endDateObject)}</Text>
           </HStack>
+        </Flex>
+        <Flex gap="4" flexWrap="wrap" mt="8">
+          <Box>
+            <Text fontWeight="600" fontSize="lg">Mentee</Text>
+            <Flex mt="2">
+              <Box padding="6" border="solid 1px" borderRadius="md" borderColor="secondary.50" display="flex" alignItems="center" gap="2">
+                <HStack spacing="4">
+                  <ProfileIcon imgUrl={sessionData?.mentee?.profileImgUrl} width="55px" height="55px" iconProps={{ fontSize: '30px' }} />
+                  <Text fontSize="lg">{sessionData?.mentee?.firstName} {sessionData?.mentee?.lastName}</Text>
+                </HStack>
+              </Box>
+            </Flex>
+          </Box>
+          <Box>
+            <Text fontWeight="600" fontSize="lg">Mentor</Text>
+            <Flex mt="2">
+              <Box padding="6" border="solid 1px" borderRadius="md" borderColor="secondary.50" display="flex" alignItems="center" gap="2">
+                <HStack spacing="4">
+                  <ProfileIcon imgUrl={sessionData?.mentor?.profileImgUrl} width="55px" height="55px" iconProps={{ fontSize: '30px' }} />
+                  <Text fontSize="lg">{sessionData?.mentor?.firstName} {sessionData?.mentor?.lastName}</Text>
+                </HStack>
+              </Box>
+            </Flex>
+          </Box>
         </Flex>
       </Container>
       <Container minHeight="calc(100vh - 32px)">
--- src/features/Feedback/questionnaires/AdminView/QuarterlyFeedbackAnswers.tsx
+++ src/features/Feedback/questionnaires/AdminView/QuarterlyFeedbackAnswers.tsx
@@ -1,6 +1,8 @@
 import { useParams } from 'react-router-dom'
-import { Text } from '@chakra-ui/react'
-import { Container } from '../../../../components'
+import {
+  Box, Flex, HStack, Text,
+} from '@chakra-ui/react'
+import { Container, ProfileIcon } from '../../../../components'
 import QuestionList from '../../components/QuestionList'
 import { MENTOR_QUARTERLY_QUESTIONS } from '../../constants'
 import { useGetMentorQuarterlyFeedbackQuery } from '../../../../app/services/feedback/apiFeedbackSlice'
@@ -15,10 +17,33 @@ function MentorQuarterlyFeedbackQuestionnaire() {
 
   return (
     <>
-      {/* TODO: Add Mentor + Mentee */}
       <Container mb="4">
-        <Text fontSize="lg" fontWeight="bold">{mentoringJourneyData?.title}</Text>
+        <Text fontSize="lg" fontWeight="bold">{feedbackData?.role === 'mentor' ? 'Mentor Feedback -' : 'Mentee Feedback -'} {mentoringJourneyData?.title}</Text>
         <Text fontSize="md" fontWeight="semi-bold">Quarter {quarter} Feedback answers</Text>
+        <Flex gap="4" flexWrap="wrap" mt="8">
+          <Box>
+            <Text fontWeight="600" fontSize="lg">Mentee</Text>
+            <Flex mt="2">
+              <Box padding="6" border="solid 1px" borderRadius="md" borderColor="secondary.50" display="flex" alignItems="center" gap="2">
+                <HStack spacing="4">
+                  <ProfileIcon imgUrl={mentoringJourneyData?.mentee?.profileImgUrl} width="55px" height="55px" iconProps={{ fontSize: '30px' }} />
+                  <Text fontSize="lg">{mentoringJourneyData?.mentee?.firstName} {mentoringJourneyData?.mentee?.lastName}</Text>
+                </HStack>
+              </Box>
+            </Flex>
+          </Box>
+          <Box>
+            <Text fontWeight="600" fontSize="lg">Mentor</Text>
+            <Flex mt="2">
+              <Box padding="6" border="solid 1px" borderRadius="md" borderColor="secondary.50" display="flex" alignItems="center" gap="2">
+                <HStack spacing="4">
+                  <ProfileIcon imgUrl={mentoringJourneyData?.mentor?.profileImgUrl} width="55px" height="55px" iconProps={{ fontSize: '30px' }} />
+                  <Text fontSize="lg">{mentoringJourneyData?.mentor?.firstName} {mentoringJourneyData?.mentor?.lastName}</Text>
+                </HStack>
+              </Box>
+            </Flex>
+          </Box>
+        </Flex>
       </Container>
       <Container>
         <QuestionList
