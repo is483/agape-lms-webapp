4e57dc18bfde019bd7eb59e95aba540b88755ec2
lanceljr <lanceljr98@gmail.com>
2024-03-16 21:32:11 +0800

    added goals dashboard

--- src/features/AdminMentoringJourneys/AdminMentoringJourneyDetails/Metrics/GoalMetrics.tsx
+++ src/features/AdminMentoringJourneys/AdminMentoringJourneyDetails/Metrics/GoalMetrics.tsx
@@ -14,8 +14,7 @@ function GoalMetrics() {
         </Text>
       </Alert>
       <Flex justifyContent="center" mt="10">
-        {/* // TODO: Update after Bong is that */}
-        <iframe width="600" height="450" title="Goals Dashboard" src="https://lookerstudio.google.com/embed/reporting/33f17403-52fa-4e85-8cb3-a1db82f54f52/page/X8esD" style={{ border: '0' }} allowFullScreen sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox" />
+        <iframe width="700" height="600" title="Goals Dashboard" src="https://lookerstudio.google.com/embed/reporting/33f17403-52fa-4e85-8cb3-a1db82f54f52/page/X8esD" style={{ borderRadius: '5px' }} allowFullScreen sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox" />
       </Flex>
     </Flex>
   )
--- src/features/AdminMentoringJourneys/AdminMentoringJourneyDetails/Metrics/SessionMetrics.tsx
+++ src/features/AdminMentoringJourneys/AdminMentoringJourneyDetails/Metrics/SessionMetrics.tsx
@@ -14,8 +14,7 @@ function SessionMetrics() {
         </Text>
       </Alert>
       <Flex justifyContent="center" mt="10">
-        {/* // TODO: Update after Bong is that */}
-        <iframe width="600" height="450" title="Sessions Dashboard" src="https://lookerstudio.google.com/embed/reporting/33f17403-52fa-4e85-8cb3-a1db82f54f52/page/X8esD" style={{ border: '0' }} allowFullScreen sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox" />
+        <iframe width="700" height="600" title="Sessions Dashboard" src="https://lookerstudio.google.com/u/0/reporting/9ef7fcb5-7d29-4d05-968c-a2ed6fe7fb2a/page/fmyrD" style={{ borderRadius: '5px' }} allowFullScreen sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox" />
       </Flex>
     </Flex>
   )
