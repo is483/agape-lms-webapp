e5d2f779e5a0208a198fddbf3ec124acd6fe1559
lanceljr <lanceljr98@gmail.com>
2024-03-23 16:41:49 +0800

    Added search filter under pairing

--- src/features/AdminPairing/Pairing.tsx
+++ src/features/AdminPairing/Pairing.tsx
@@ -1,6 +1,7 @@
 import {
-  Box, Divider, Flex, Text, HStack, Circle, Image, Hide,
+  Box, Divider, Flex, Text, HStack, Circle, Image, Hide, InputGroup, InputLeftElement, Input,
 } from '@chakra-ui/react'
+import { ChangeEvent, useState } from 'react'
 import { Container, Icon } from '../../components'
 import { useGetAllMentorsAdminQuery, useGetUnAssignedMenteesAdminQuery } from '../../app/services/user/apiUserSlice'
 import MentorPairingCard from './components/MentorPairingCard'
@@ -9,9 +10,15 @@ import { MentorsAdminResponse } from '../../app/services/user/types'
 function Pairing() {
   const { data: unAssignedMenteesData } = useGetUnAssignedMenteesAdminQuery(null)
   const { data: allMentorData } = useGetAllMentorsAdminQuery(null)
+  const [searchTerm, setSearchTerm] = useState('')
   const isUnassignedMenteesEmpty = !unAssignedMenteesData || unAssignedMenteesData.length === 0
 
-  // useState for search value
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(e.target.value)
+  }
+  const filteredMentorData = allMentorData?.filter((mentor: MentorsAdminResponse) => `${mentor.mentor.firstName} ${mentor.mentor.lastName}`.toLowerCase().includes(searchTerm.toLowerCase()),
+  )
+
   return (
     <Container minHeight="calc(100vh - 32px)">
       <Box padding="5">
@@ -21,8 +28,17 @@ function Pairing() {
       <Divider />
       <Flex padding="5">
         <Box flex="4">
-          <Text mt="5" fontWeight="bold" fontSize="xl"> Mentors ({allMentorData?.length})</Text>
-          {allMentorData?.map((mentorData: MentorsAdminResponse) => (
+          <Text marginY="5" fontWeight="bold" fontSize="xl"> Mentors ({allMentorData?.length})</Text>
+          <Box maxW="lg">
+            <InputGroup>
+              <InputLeftElement pointerEvents="none">
+                <Icon color="secondary.500" name="search" />
+              </InputLeftElement>
+              <Input type="text" value={searchTerm} onChange={handleInputChange} placeholder="Search by mentor name" />
+            </InputGroup>
+          </Box>
+
+          {filteredMentorData?.map((mentorData: MentorsAdminResponse) => (
             <MentorPairingCard mentorInfo={mentorData} isUnassignedMenteesEmpty={isUnassignedMenteesEmpty} />
           ))}
         </Box>
